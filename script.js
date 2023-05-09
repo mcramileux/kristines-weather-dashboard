6037b5849b973da14f203f7c25a98e2e0be03c40
mcramileux <kristineramilo21@gmail.com>
2023-05-10 00:12:52 +1000

    fixed js format date

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function formatDate(ts) {
+    var date = new Date(ts);  
+    var day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    var month = date.getMonth()+1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    var year = date.getFullYear();
+    return day + "/" + month +"/" + year;
+}
+
 function searchCity() {
     var searchedCity = document.querySelector("#cityName")
     var searchCity = document.querySelector("#cityName").value
@@ -45,103 +59,48 @@ function searchCity() {
                 })
                 .then(function (data) {
                     console.log(data)
-                    document.getElementById("date-1").textContent = "Date: " + data.list[1].dt_txt 
+                    document.getElementById("date-1").textContent = "Date: " + formatDate(data.list[1].dt_txt);
                     document.getElementById("temperature-1").textContent = "Temp: " + data.list[1].main.temp + "°C"
                     document.querySelector("#wind-1").textContent = "Wind: " + data.list[1].wind.speed + "kph"
                     document.querySelector("#humidity-1").textContent = "Humidity: " + data.list[1].main.humidity + "%"
                     document.querySelector("#icon-1").src = "https://openweathermap.org/img/wn/"+data.list[1].weather[0].icon+"@2x.png"
 
-                    document.getElementById("date-2").textContent = "Date: " + data.list[9].dt_txt
+                    document.getElementById("date-2").textContent = "Date: " + formatDate(data.list[9].dt_txt);
                     document.getElementById("temperature-2").textContent = "Temp: " + data.list[9].main.temp + "°C"
                     document.querySelector("#wind-2").textContent = "Wind: " + data.list[9].wind.speed + "kph"
                     document.querySelector("#humidity-2").textContent = "Humidity: " + data.list[9].main.humidity + "%"
                     document.querySelector("#icon-2").src = "https://openweathermap.org/img/wn/"+data.list[9].weather[0].icon+"@2x.png"
 
-                    document.getElementById("date-3").textContent = "Date: " + data.list[17].dt_txt
+                    document.getElementById("date-3").textContent = "Date: " + formatDate(data.list[17].dt_txt);
                     document.getElementById("temperature-3").textContent = "Temp: " + data.list[17].main.temp + "°C"
                     document.querySelector("#wind-3").textContent = "Wind: " + data.list[17].wind.speed + "kph"
                     document.querySelector("#humidity-3").textContent = "Humidity: " + data.list[17].main.humidity + "%"
                     document.querySelector("#icon-3").src = "https://openweathermap.org/img/wn/"+data.list[17].weather[0].icon+"@2x.png"
 
-                    document.getElementById("date-4").textContent = "Date: " + data.list[25].dt_txt
+                    document.getElementById("date-4").textContent = "Date: " + formatDate(data.list[25].dt_txt);
                     document.getElementById("temperature-4").textContent = "Temp: " + data.list[25].main.temp + "°C"
                     document.querySelector("#wind-4").textContent = "Wind: " + data.list[25].wind.speed + "kph"
                     document.querySelector("#humidity-4").textContent = "Humidity: " + data.list[25].main.humidity + "%"
                     document.querySelector("#icon-4").src = "https://openweathermap.org/img/wn/"+data.list[25].weather[0].icon+"@2x.png"
 
-                    document.getElementById("date-5").textContent = "Date: " + data.list[33].dt_txt
+                    document.getElementById("date-5").textContent = "Date: " + formatDate(data.list[33].dt_txt);
                     document.getElementById("temperature-5").textContent = "Temp: " + data.list[33].main.temp + "°C"
                     document.querySelector("#wind-5").textContent = "Wind: " + data.list[33].wind.speed + "kph"
                     document.querySelector("#humidity-5").textContent = "Humidity: " + data.list[33].main.humidity + "%"
                     document.querySelector("#icon-5").src = "https://openweathermap.org/img/wn/"+data.list[33].weather[0].icon+"@2x.png"
-
+                    document.querySelector(".begin").classList.remove("begin");
                 })
         })
 }
 
-// function getCityForecast() {
 
-// }
-
-// function getCurrentForecast() {
-
-// }
 document.querySelector(".btn").addEventListener("click", searchCity)
 
 document.querySelector("#cityName").addEventListener("keydown", function (e) {
     if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
         e.preventDefault();
         searchCity()
+        
     }
 });
 
-//Get the elements to remove the class from
-// var currentResult = document.querySelector('.currentResultPanel');
-// var forecastColumn = document.querySelector('.forecastColumn');
-// var footer = document.querySelector('.footer');
-
-// searchCity();
-// document.querySelector(".currentResultPanel").style = undefined;
-// document.querySelector(".forecastColumn").style = undefined;
-// document.querySelector(".footer").style = undefined;
-
-//formatting dates using moment.js
-
-//Searching for newCitySearch
-// function newCitySearch () {
-
-// }
-
-// //construct currentWeatherUrl using searchCity and apiKey
-// // fetch currentWeatherUrl
-// //     .then parse JSON response
-// //     .then process and display weather data
-
-// //To fix this, you need to set the display style of these elements to "block" after a search is made.
-// // Pseudocode:
-// // set display style of currentResultPanel to "block"
-// // set display style of forecastColumn to "block"
-// // set display style of footer to "block"
-
-// // Define variables for the result panel, forecast column, and footer
-// const currentResultPanel = document.getElementById("current-result-panel");
-// const forecastColumn = document.getElementById("forecast-column");
-// const footer = document.getElementById("footer");
-
-// // Set the display style of the elements to "none" initially
-// currentResultPanel.style.display = "none";
-// forecastColumn.style.display = "none";
-// footer.style.display = "none";
-
-// // When a search is made, set the display style of the elements to "block"
-// function handleSearch() {
-//   currentResultPanel.style.display = "block";
-//   forecastColumn.style.display = "block";
-//   footer.style.display = "block";
-
-//   // Add code here to handle the search
-// }
-
-// // Add an event listener to the search button to call the handleSearch function when clicked
-// const searchButton = document.getElementById("search-button");
-// searchButton.addEventListener("click", handleSearch);
